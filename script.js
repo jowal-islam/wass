00815652ba4c7ed3676fe6c7561362ca123003db
jowal-islam <ranabrindabon@gmail.com>
2025-09-07 23:50:09 +0600

    main section card add

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
+const loadCatagorys = () => {
+  const url = "https://openapi.programming-hero.com/api/categories";
+  fetch(url)
+    .then((res) => res.json())
+    .then((json) => showCatagory(json));
+};
+loadCatagorys();
 
-const loadCatagorys = ()=> {
-    const url = "https://openapi.programming-hero.com/api/categories"
-    fetch(url)
-    .then(res => res.json())
-    .then(json => showCatagory(json))
-}
-loadCatagorys()
-
-const showCatagory = (datas)=> {
+const showCatagory = (datas) => {
   const categories = datas.categories;
-  categories.forEach(cat => {
-   
-    const categoriesDiv = document.getElementById('Categories')
-    const catChild = document.createElement('h1')
-    catChild.innerText = cat.category_name
-     
+  categories.forEach((cat) => {
+    const categoriesDiv = document.getElementById("Categories");
+    const btn = document.createElement("button");
 
+    btn.innerText = cat.category_name;
+    btn.style.display = "block";
+    btn.style.margin = "5px";
+    btn.style.padding = "10px";
+    btn.style.borderRadius = "5px";
+    btn.style.cursor = "pointer";
 
-    categoriesDiv.appendChild(catChild)
-  })
-}
+    btn.addEventListener("click", () => {
+         alert(`You clicked on: ${cat.category_name}`);
+    });
+
+    categoriesDiv.appendChild(btn);
+  });
+};
