72060db08c5a18d5881b7fe075874d89fb30f682
jowal-islam <ranabrindabon@gmail.com>
2025-09-07 22:48:22 +0600

    catagory show the display

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 
-console.log('connected')
+const loadCatagorys = ()=> {
+    const url = "https://openapi.programming-hero.com/api/categories"
+    fetch(url)
+    .then(res => res.json())
+    .then(json => showCatagory(json))
+}
+loadCatagorys()
+
+const showCatagory = (datas)=> {
+  const categories = datas.categories;
+  categories.forEach(cat => {
+   
+    const categoriesDiv = document.getElementById('Categories')
+    const catChild = document.createElement('h1')
+    catChild.innerText = cat.category_name
+     
+
+
+    categoriesDiv.appendChild(catChild)
+  })
+}
