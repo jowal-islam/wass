d598b19eb795603f159554eba84f972e9deea9b6
Jowal Islam <ranabrindabon@gmail.com>
2025-09-08 15:00:47 +0600

    ui setup for api data load

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// all categories
 const loadCatagorys = () => {
   const url = "https://openapi.programming-hero.com/api/categories";
   fetch(url)
@@ -6,6 +7,68 @@ const loadCatagorys = () => {
 };
 loadCatagorys();
 
+// all plants
+const loadAllPlants = () => {
+  const url = "https://openapi.programming-hero.com/api/plants"
+  fetch(url)
+  .then(res => res.json())
+  .then(json => showAllPlants(json))
+}
+loadAllPlants()
+// load catagory plants
+// const loadCatagorysplants = () => {
+//   const url = "https://openapi.programming-hero.com/api/category/1"
+//   fetch(url)
+//   .then(res => res.json())
+//    .then(json => showCatagoryPlants(json) )
+// }
+// loadCatagorysplants()
+
+//  show Catagorys ui 
+// const showCatagoryPlants = (datas)=>{
+//   const plants = datas.plants
+//   plants.forEach(data =>{
+//     console.log(data)
+//   } )
+// }
+
+// show all Plants
+const showAllPlants = (datas)=>{
+  const plants = datas.plants
+  const palntsContainer = document.getElementById('plants')
+  palntsContainer.innerHTML = "";
+ plants.forEach(plant => {
+  //  console.log(plant)
+   const divContainer = document.createElement('div')
+   divContainer.innerHTML = `
+      
+      <div class="card bg-base-100  shadow-sm">
+          <figure>
+            <img
+              src= "${plant.image}"
+              class="w-60 h-60 object-cover rounded-sm mt-5 "
+              alt="Shoes"
+            />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">${plant.category}</h2>
+            <p>
+              ${plant.description}
+            </p>
+            <h1> price :${plant.price}</h1>
+          
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary w-full">Buy Now</button>
+            </div>
+          </div>
+        </div> 
+      
+   `
+   palntsContainer.appendChild(divContainer)
+ })
+}
+
+// show category_name
 const showCatagory = (datas) => {
   const categories = datas.categories;
   categories.forEach((cat) => {
@@ -19,9 +82,16 @@ const showCatagory = (datas) => {
     btn.style.borderRadius = "5px";
     btn.style.cursor = "pointer";
 
-    btn.addEventListener("click", () => {
-         alert(`You clicked on: ${cat.category_name}`);
-    });
+    btn.addEventListener("click", ()=> {
+        const url = "https://openapi.programming-hero.com/api/category/1";
+
+      fetch(url)
+        .then(res => res.json())
+        .then(json => console.log(json))
+        // .then(json => showAllPlants(json)) 
+       
+        .catch(err => console.error("Error:", err));
+    })  
 
     categoriesDiv.appendChild(btn);
   });
