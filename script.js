b9fba1c070766e69df4fbd3787af5ac8865c09ee
jowal-islam <ranabrindabon@gmail.com>
2025-09-08 17:55:21 +0600

    catagory btn update

--- script.js
+++ script.js
@@ -15,22 +15,6 @@ const loadAllPlants = () => {
     .then((json) => showAllPlants(json));
 };
 loadAllPlants();
-// load catagory plants
-// const loadCatagorysplants = () => {
-//   const url = "https://openapi.programming-hero.com/api/category/1"
-//   fetch(url)
-//   .then(res => res.json())
-//    .then(json => showCatagoryPlants(json) )
-// }
-// loadCatagorysplants()
-
-//  show Catagorys ui
-// const showCatagoryPlants = (datas)=>{
-//   const plants = datas.plants
-//   plants.forEach(data =>{
-//     console.log(data)
-//   } )
-// }
 
 // show all Plants
 const showAllPlants = (datas) => {
@@ -71,7 +55,7 @@ const showAllPlants = (datas) => {
 // show category_name
 const showCatagory = (datas) => {
   const categories = datas.categories;
-  categories.forEach((cat) => {
+  categories.forEach((cat, index) => {
     const categoriesDiv = document.getElementById("Categories");
     const btn = document.createElement("button");
 
@@ -82,28 +66,128 @@ const showCatagory = (datas) => {
     btn.style.borderRadius = "5px";
     btn.style.cursor = "pointer";
 
-    btn.addEventListener("click", () => {
-      const url = "https://openapi.programming-hero.com/api/plants";
 
-      fetch(url)
-        .then((res) => res.json())
-        // .then(json => console.log(json))
-        .then((json) => showAllnewPlants(json))
+// add btn for categories control
+    btn.addEventListener("click", (event) => {
+      console.log("Clicked Category:", cat.category_name); // কোন category তে ক্লিক হলো
+      console.log("Category Index:", index); // কত নম্বর index সেটা
+      // if category_name == "fruit tree" this code is run
 
-        .catch((err) => console.error("Error:", err));
-      const palntsContainer = document.getElementById("plants");
-      palntsContainer.innerHTML = "";
+      if (cat.category_name === "Fruit Tree") {
+        const url = "https://openapi.programming-hero.com/api/plants";
+        fetch(url)
+          .then((res) => res.json())
+          .then((json) => showFruitTree(json));
+        const palntsContainer = document.getElementById("plants");
+        palntsContainer.innerHTML = "";
+
+      } else if (cat.category_name === "Flowering Tree") {
+        const url = "https://openapi.programming-hero.com/api/plants";
+        fetch(url)
+          .then((res) => res.json())
+          .then((json) => showFlowering(json));
+        const palntsContainer = document.getElementById("plants");
+        palntsContainer.innerHTML = "";
+
+      } else if(cat.category_name === "Shade Tree"){
+          const url = "https://openapi.programming-hero.com/api/plants";
+        fetch(url)
+          .then((res) => res.json())
+          .then((json) => showShadeTree(json));
+        const palntsContainer = document.getElementById("plants");
+        palntsContainer.innerHTML = "";
+      }
+
     });
 
     categoriesDiv.appendChild(btn);
   });
 };
 
-const showAllnewPlants = (datas) => {
+
+
+// show fruit tree
+const showFruitTree = (datas) => {
   const palntsContainer = document.getElementById("plants");
   const plants = datas.plants;
   plants.forEach((plant) => {
     if (plant.category === "Fruit Tree") {
+      console.log(plant);
+      const divContainer = document.createElement("div");
+      divContainer.innerHTML = `
+      
+      <div class="card bg-base-100  shadow-sm">
+          <figure>
+            <img
+              src= "${plant.image}"
+              class="w-60 h-60 object-cover rounded-sm mt-5 "
+              alt="Shoes"
+            />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">${plant.name}</h2>
+            <p>
+              ${plant.description}
+            </p>
+            <h1> price :${plant.price}</h1>
+          
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary w-full">Buy Now</button>
+            </div>
+          </div>
+        </div> 
+      
+   `;
+      palntsContainer.appendChild(divContainer);
+    }
+  });
+};
+
+// Flowering Tree ui
+
+const showFlowering = (datas) => {
+  const palntsContainer = document.getElementById("plants");
+  const plants = datas.plants;
+  plants.forEach((plant) => {
+    if (plant.category === "Flowering Tree") {
+      console.log(plant);
+      const divContainer = document.createElement("div");
+      divContainer.innerHTML = `
+      
+      <div class="card bg-base-100  shadow-sm">
+          <figure>
+            <img
+              src= "${plant.image}"
+              class="w-60 h-60 object-cover rounded-sm mt-5 "
+              alt="Shoes"
+            />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">${plant.name}</h2>
+            <p>
+              ${plant.description}
+            </p>
+            <h1> price :${plant.price}</h1>
+          
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary w-full">Buy Now</button>
+            </div>
+          </div>
+        </div> 
+      
+   `;
+      palntsContainer.appendChild(divContainer);
+    }
+  });
+};
+
+// shade tree ui
+
+const showShadeTree = (datas) => {
+  const palntsContainer = document.getElementById("plants");
+  const plants = datas.plants;
+  plants.forEach((plant) => {
+    if (plant.category === "Shade Tree") {
       console.log(plant);
       const divContainer = document.createElement("div");
       divContainer.innerHTML = `
